0557e46b16292df23ca01e85b885b57fc599daad
sergigt10 <sergigenero@gmail.com>
2021-01-07 13:33:04 +0100

    petits canvis

--- app/views/immobles/cercar.php
+++ app/views/immobles/cercar.php
@@ -55,17 +55,17 @@
 										</li>
 										<li>
 											<div class="small_dropdown2">
-											    <div id="prncgs" class="btn dd_btn">
-											    	<span>Price</span>
-											    	<label for="exampleInputEmail2"><span class="fa fa-angle-down"></span></label>
-											    </div>
-											  	<div class="dd_content2">
-												    <div class="pricing_acontent">
+												<div id="prncgs" class="btn dd_btn">
+													<span>Price</span>
+													<label for="exampleInputEmail2"><span class="fa fa-angle-down"></span></label>
+												</div>
+												<div class="dd_content2">
+													<div class="pricing_acontent">
 														<input type="text" class="amount" placeholder="$52,239"> 
 														<input type="text" class="amount2" placeholder="$985,14">
 														<div class="slider-range"></div>
-												    </div>
-											  	</div>
+													</div>
+												</div>
 											</div>
 										</li>
 										<li>
@@ -128,129 +128,129 @@
 											</div>
 										</li>
 										<li class="min_area style2 list-inline-item">
-										    <div class="form-group">
-										    	<input type="text" class="form-control" id="exampleInputName2" placeholder="Min Area">
-										    </div>
+											<div class="form-group">
+												<input type="text" class="form-control" id="exampleInputName2" placeholder="Min Area">
+											</div>
 										</li>
 										<li class="max_area list-inline-item">
-										    <div class="form-group">
-										    	<input type="text" class="form-control" id="exampleInputName3" placeholder="Max Area">
-										    </div>
+											<div class="form-group">
+												<input type="text" class="form-control" id="exampleInputName3" placeholder="Max Area">
+											</div>
 										</li>
 										<li>
-										  	<div id="accordion" class="panel-group">
-											    <div class="panel">
-											      	<div class="panel-heading">
-												      	<h4 class="panel-title">
-												        	<a href="#panelBodyRating" class="accordion-toggle link" data-toggle="collapse" data-parent="#accordion"><i class="flaticon-more"></i> Advanced features</a>
-												        </h4>
-											      	</div>
-												    <div id="panelBodyRating" class="panel-collapse collapse">
-												        <div class="panel-body row">
-												      		<div class="col-lg-12">
-												                <ul class="ui_kit_checkbox selectable-list float-left fn-400">
-												                	<li>
+											<div id="accordion" class="panel-group">
+												<div class="panel">
+													<div class="panel-heading">
+														<h4 class="panel-title">
+															<a href="#panelBodyRating" class="accordion-toggle link" data-toggle="collapse" data-parent="#accordion"><i class="flaticon-more"></i> Advanced features</a>
+														</h4>
+													</div>
+													<div id="panelBodyRating" class="panel-collapse collapse">
+														<div class="panel-body row">
+															<div class="col-lg-12">
+																<ul class="ui_kit_checkbox selectable-list float-left fn-400">
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck1">
 																			<label class="custom-control-label" for="customCheck1">Air Conditioning</label>
 																		</div>
-												                	</li>
-												                	<li>
+																	</li>
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck4">
 																			<label class="custom-control-label" for="customCheck4">Barbeque</label>
 																		</div>
-												                	</li>
-												                	<li>
+																	</li>
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck10">
 																			<label class="custom-control-label" for="customCheck10">Gym</label>
 																		</div>
-												                	</li>
-												                	<li>
+																	</li>
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck5">
 																			<label class="custom-control-label" for="customCheck5">Microwave</label>
 																		</div>
-												                	</li>
-												                	<li>
+																	</li>
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck6">
 																			<label class="custom-control-label" for="customCheck6">TV Cable</label>
 																		</div>
-												                	</li>
-												                	<li>
+																	</li>
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck2">
 																			<label class="custom-control-label" for="customCheck2">Lawn</label>
 																		</div>
-												                	</li>
-												                	<li>
+																	</li>
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck11">
 																			<label class="custom-control-label" for="customCheck11">Refrigerator</label>
 																		</div>
-												                	</li>
-												                	<li>
+																	</li>
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck3">
 																			<label class="custom-control-label" for="customCheck3">Swimming Pool</label>
 																		</div>
-												                	</li>
-												                </ul>
-												                <ul class="ui_kit_checkbox selectable-list float-right fn-400">
-												                	<li>
+																	</li>
+																</ul>
+																<ul class="ui_kit_checkbox selectable-list float-right fn-400">
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck12">
 																			<label class="custom-control-label" for="customCheck12">WiFi</label>
 																		</div>
-												                	</li>
-												                	<li>
+																	</li>
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck14">
 																			<label class="custom-control-label" for="customCheck14">Sauna</label>
 																		</div>
-												                	</li>
-												                	<li>
+																	</li>
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck7">
 																			<label class="custom-control-label" for="customCheck7">Dryer</label>
 																		</div>
-												                	</li>
-												                	<li>
+																	</li>
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck9">
 																			<label class="custom-control-label" for="customCheck9">Washer</label>
 																		</div>
-												                	</li>
-												                	<li>
+																	</li>
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck13">
 																			<label class="custom-control-label" for="customCheck13">Laundry</label>
 																		</div>
-												                	</li>
-												                	<li>
+																	</li>
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck8">
 																			<label class="custom-control-label" for="customCheck8">Outdoor Shower</label>
 																		</div>
-												                	</li>
-												                	<li>
+																	</li>
+																	<li>
 																		<div class="custom-control custom-checkbox">
 																			<input type="checkbox" class="custom-control-input" id="customCheck15">
 																			<label class="custom-control-label" for="customCheck15">Window Coverings</label>
 																		</div>
-												                	</li>
-												                </ul>
-													        </div>
-												        </div>
-												    </div>
-											    </div>
+																	</li>
+																</ul>
+															</div>
+														</div>
+													</div>
+												</div>
 											</div>
 										</li>
 										<li>
 											<div class="search_option_button">
-											    <button type="submit" class="btn btn-block btn-thm">Search</button>
+												<button type="submit" class="btn btn-block btn-thm">Search</button>
 											</div>
 										</li>
 									</ul>
@@ -264,8 +264,8 @@
 				<div class="col-md-8 col-lg-6">
 					<div class="breadcrumb_content style2">
 						<ol class="breadcrumb">
-						    <li class="breadcrumb-item"><a href="<?php echo URLROOT; ?>/immobles/index">Inici</a></li>
-						    <li class="breadcrumb-item active text-thm" aria-current="page">Immobles</li>
+							<li class="breadcrumb-item"><a href="<?php echo URLROOT; ?>/immobles/index">Inici</a></li>
+							<li class="breadcrumb-item active text-thm" aria-current="page">Immobles</li>
 						</ol>
 						<h2 class="breadcrumb_title">Resultats de la cerca: </h2>
 					</div>
