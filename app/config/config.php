44a1a4f5abca5acf5a86e2c5c341fd24102228d6
sergigt10 <phxhollow16@hotmail.com>
2021-05-26 17:51:54 +0200

    retocs

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
   define('DB_PASS', '');
-  define('DB_NAME', 'enxarxa46_admin');
+  define('DB_NAME', 'nomBD');
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
